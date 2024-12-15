c404dfe2209355a7209f059f9065c01be599e40b
E4OS Tech <e4os.tech@gmail.com>
2024-12-15 18:02:19 -0500

    UPDATE: Added new social media icons, clickable buttons that lead to each social media page added adjustability for the ko-fi icon.
    TODO:
    Configure colors/aesthetics, get php for email
    plug-ins.

--- e4script.js
+++ e4script.js
@@ -2,9 +2,8 @@ document.addEventListener("DOMContentLoaded", function () {
   // Map of social media icons to their respective URLs
   const socialMediaLinks = {
     "facebook-icon": "https://www.facebook.com/yourPage",
-    "twitter-icon": "https://twitter.com/yourProfile",
     "instagram-icon": "https://www.instagram.com/e4os.tech/",
-    "linkedin-icon": "https://www.linkedin.com/in/yourProfile",
+    "linkedin-icon": "https://www.linkedin.com/in/ty-belair-494249103/",
   };
 
   // Add event listeners to each social media icon
